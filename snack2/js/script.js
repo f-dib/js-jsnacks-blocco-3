a47c443efc28a63ce281a015e215a43dfc93ce1f
federico-dibitonto <f.dibitonto94@gmail.com>
2024-02-12 17:21:14 +0100

    second snack

---
+++ snack2/js/script.js
@@ -0,0 +1,20 @@
+let resultElement = document.querySelector("#result");
+let result = [];
+let firstArray = [1, 2, 3, 4, 5];
+let secondArray = ["a", "b", "c", "d", "e"];
+
+console.log(firstArray);
+console.log(secondArray);
+
+
+function combineArray (first, second) {
+
+    for (i = 0; i < firstArray.length; i++) {
+        result.push(first[i], second[i]);
+    };
+    
+    return result;
+
+}
+
+resultElement.innerText = combineArray (firstArray, secondArray);
